13d4744cf4d51796462cc6bea75836a05f5a24c1
azril ahsani azhar_Alprog-C <azrilazhar36@gmail.com>
2025-11-13 18:33:35 +0800

    Add files via upload

---
+++ meet   2.py
@@ -0,0 +1,35 @@
+#Variable adalah cara menyimpan nilai atau value (aljabar)
+#x: variable =: assigment 13: nilai (x=3)
+# seperti
+x = 24
+y = 30
+lebar = 50
+print("niali x adalah :", x)
+print("niali y adalah :", y)
+print("niali lebar adalah :", lebar)
+# ATURAN PENAMAAN VARIABLE
+# tidak boleh diawali angka
+xxx10 = 100
+print("nilai dari xxx10 adalah :", xxx10)
+# jika lebih dari satu kata, gabung dengan_
+Azril_GG = 526
+print("niali dari Azril_GG :", Azril_GG)
+# bisa juga menggabungkan dengan huruf kpital
+AzrilGg = 300
+print("niali dari AzrilGg:", AzrilGg)
+# PEMENGGALAN INDERECT
+x = 321
+z = x
+print("niali dari z :",z)
+# tugas
+# nama saya adalah = (nama anda)
+# nim saya adalah = (nim anda)
+a = "azril ahsani azhar"
+b = 25241091
+print("nama saya adalah :", a)
+print("nim saya adalah :", b)
+
+
+
+
+	
