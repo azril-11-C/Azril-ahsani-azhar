2403b2431907195318dd718a2f37e939ca70d1d2
azril ahsani azhar_Alprog-C <azrilazhar36@gmail.com>
2025-11-13 21:15:29 +0800

    Add variable assignments and print statements

---
+++ meet3.py
@@ -0,0 +1,28 @@
+# variabel = cara kita menyimpan nilai/velue(aljabar)
+# nilai x adalah variabel dan = adalah assigment dan 13 itu nilai value 
+
+x=
+y=
+lebar=
+
+print ("nilai x adalah: ", x)
+print ("nilai y adalah: ", y)
+print "(nilai lebar adalah: ",lebar)
+
+#   ATURAN PENAMAAN VARIABEL
+xxx10 = 500 # tidak boleh diawali angka
+azril_gg = 500 # jika lebih dari satu kata 1 kata, gabung depan_
+jumlahhitung = 1000000 # bedakan dengan kapital
+
+# panggilan inderect
+z=x
+print(z)
+print("nilai z adalaha: ",z)
+print("nilai y adalah: ",y)
+print("nilai lebar adalah: ",lebar)
+
+
+a = "azril_gg"
+b = "25240191
+print("nama saya adalah: azril_gg",)
+print("nim saya adalah: 25240191",)
