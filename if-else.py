4d2fad4f1660a2402c6d906c964fb54107ed1ddd
azril ahsani azhar_Alprog-C <azrilazhar36@gmail.com>
2026-04-13 23:44:56 +0800

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,15 @@
+# if
+# else
+# elif
+
+nama = input("masukkan nama")
+NIM = input("masukkan PIN NIM")
+
+if nama =="azril" :
+    NIM =="11"
+    print("Hai saya azril")
+    print("NIM saya 25241091")
+else : 
+    print("Mohon maaf anda bukan azril")
+    print("NIM anda tidak bisa diakses")    
+print("program selesai")
